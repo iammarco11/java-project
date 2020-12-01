ce9220fcd3b2c4a73ecafb66c86ae0f449ee37e0
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2020-12-01 18:36:16 +0530

    Userpage: Make the buttons functional This commit does: -Prints a statement when the button is clicked, future possiblity of more methods

--- src/project/UserPage.java
+++ src/project/UserPage.java
@@ -14,10 +14,10 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-public class UserPage implements ActionListener {
+public class UserPage {
 	
-	JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
+	static JFrame frame = new JFrame();
+	static JPanel panel = new JPanel();
 	
 	private static JButton checkTax;
 	private static JButton updateCredentials;
@@ -44,23 +44,58 @@ public class UserPage implements ActionListener {
 		panel.setLayout(new FlowLayout(FlowLayout.LEFT,10,10));
 		
 		checkTax = new JButton("Check tax");
+		checkTax.addActionListener(new CheckTax());
 		panel.add(checkTax);
 		
 		updateCredentials = new JButton("Update Credentials");
+		updateCredentials.addActionListener(new UpdateCredentials());
 		panel.add(updateCredentials);
 		
 		viewIncome = new JButton("View Income");
+		viewIncome.addActionListener(new ViewIncome());
 		panel.add(viewIncome);
 		
 		viewInvestment = new JButton("View Investment");
+		viewInvestment.addActionListener(new ViewInvestment());
 		panel.add(viewInvestment);
 		
 		taxCalculator = new JButton("Calculate Tax");
+		taxCalculator.addActionListener(new TaxCalculator());
 		panel.add(taxCalculator);
     }
 
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	static class CheckTax implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println("Check Tax button clicked");
+		}
+	}
+	
+	static class UpdateCredentials implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println("Update Credentials button clicked");
+		}
+	}
+	
+	static class ViewIncome implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println("View Income button clicked");
+		}
+	}
+	
+	static class ViewInvestment implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println("View Investment button clicked");
+		}
+	}
+	
+	static class TaxCalculator implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println("Tax Calulator button clicked");
+		}
 	}
 }

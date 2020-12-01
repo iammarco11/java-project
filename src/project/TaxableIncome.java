30f2b70186335aa486caa79b9df188b02a871f37
Giwansh Aryan <giwansharyan@pop-os.localdomain>
2020-12-01 19:07:56 +0530

    Added Functions

--- src/project/TaxableIncome.java
+++ src/project/TaxableIncome.java
@@ -2,4 +2,24 @@ package project;
 
 public class TaxableIncome {
 
+	double TotalIncome;
+	double TotalInvestment;
+	double Rate;
+	String Slab;
+	double TaxAmount;
+	
+	public static double CalculateTax(){
+	
+	};
+	public static double ReturnTaxAmt(){
+	
+	};
+	public static double ValidateTax(){
+	
+	};
+	public static double TaxDue(){
+	
+	};
+	
+
 }

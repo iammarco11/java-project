972283bd977ad903bbcea41265510aba87825690
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2020-12-01 21:40:10 +0530

    Taxableincome: Initialise all the methods and class variables

--- src/project/TaxableIncome.java
+++ src/project/TaxableIncome.java
@@ -1,5 +1,32 @@
 package project;
 
 public class TaxableIncome {
-
+	String AadhaarNo;
+	double TotalIncome;
+	double TotalInvestment;
+	double Rate;
+	String Slab;
+	double TaxAmount;
+	
+	public TaxableIncome(String AadhaarNo) {
+		this.AadhaarNo = AadhaarNo;
+	}
+	
+	public double Calculatetax() {
+		return TaxAmount;
+	}
+	
+	public double ReturnTaxAmt() {
+		return TaxAmount;
+	}
+	
+	public double ValidateTax() {
+		return TaxAmount;
+	}
+	
+	public double TaxDue() {
+		return TaxAmount;
+	}
+	
+	
 }

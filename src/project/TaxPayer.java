f7490e2435e7dcfaee26b7848816ece7f7222272
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2020-12-01 19:55:36 +0530

    Taxpayer: Initialise all the methods and class variables

--- src/project/TaxPayer.java
+++ src/project/TaxPayer.java
@@ -1,5 +1,31 @@
 package project;
 
 public class TaxPayer {
-
+	
+	String Name;
+	String PanNo;
+	String AadhaarNo;
+	String Address;
+	double PhoneNo;
+	
+	
+	public TaxPayer(String AadhaarNo){
+		this.AadhaarNo = AadhaarNo;
+	}
+	
+	public void CreateNewPayer(String Name, String PanNo, String AadhaarNo, String Address, double PhoneNo){
+		
+	}
+	
+	public void ValidatePayer(String AadhaarNo){
+		
+	}
+	
+	public void FindPayer(String AadhaarNo){
+		
+	}
+	
+	public void ChangeCredentials(String Name, String PanNo, String AadhaarNo, String Address, double PhoneNo){
+		
+	}
 }

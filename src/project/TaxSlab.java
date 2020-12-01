a383b42350285028aa728765b5b9cf3a10513426
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2020-12-01 20:05:35 +0530

    Taxslab: Initialise all the methods and class variables

--- src/project/TaxSlab.java
+++ src/project/TaxSlab.java
@@ -1,5 +1,27 @@
 package project;
 
 public class TaxSlab {
-
+	
+	String AadhaarNo;
+	String Slab;
+	double Rate;
+	double MinAmt;
+	double MaxAmt;
+	
+	public TaxSlab(String AadhaarNo) {
+		this.AadhaarNo = AadhaarNo;		
+	}
+	
+	public double ReturnRate() {
+		return this.Rate;
+	}
+	
+	public String ReturnSlab() {
+		return this.Slab;
+	}
+	
+	public double RateCalc() {
+		return this.Rate;
+	}
+	
 }

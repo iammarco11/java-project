0f0d437515210f42d7e306ceab32071df16ca547
Giwansh Aryan <giwansharyan@pop-os.localdomain>
2020-12-01 18:57:01 +0530

    Added functions

--- src/project/Investment.java
+++ src/project/Investment.java
@@ -2,4 +2,26 @@ package project;
 
 public class Investment {
 
+	double HousingLoan;
+	double OtherLoan;
+	double Rent;
+	double ProvidentFund;
+	double Policies;
+	double TotalInvestment;
+	
+	public static double (){
+	
+	
+	};
+	public static void EditCredentials(){
+	
+	};
+	public static void(){
+	
+	
+	};
+	public static double(){
+	
+	};
+
 }

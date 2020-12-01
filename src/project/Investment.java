1b37a62712cf85c794ee07ef0012f52636ec856a
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2020-12-01 22:02:37 +0530

    Investment: Initialise all the methods and class variables

--- src/project/Investment.java
+++ src/project/Investment.java
@@ -1,5 +1,33 @@
 package project;
 
 public class Investment {
-
+	
+	String AadhaarNo;
+	double HousingLoan;
+	double OtherLoan;
+	double Rent;
+	double ProvidedFund;
+	double Policies;
+	double TotalInvestment;
+	
+	
+	public Investment(String AadhaarNo) {
+		this.AadhaarNo = AadhaarNo;
+	}
+	
+	public double ReturnInvestment() {
+		return TotalInvestment;
+	}
+	
+	public void EditCredentials() {
+		
+	}
+	
+	public void Validate() {
+		
+	}
+	
+	public double Calculation() {
+		return TotalInvestment;
+	}
 }

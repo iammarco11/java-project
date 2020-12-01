1b916fb2e132c6658ce4be50fc471517cf1eeac4
Giwansh Aryan <giwansharyan@pop-os.localdomain>
2020-12-01 18:48:07 +0530

    Added functions

--- src/project/Income.java
+++ src/project/Income.java
@@ -2,4 +2,25 @@ package project;
 
 public class Income {
 
+	double Job;
+	double Business;
+	double Property;
+	double TotalIncome;
+	
+	public static double ReturnIncome(){
+	
+	
+	};
+	public static double Calculation(){
+	
+	
+	};
+	public static void EditCredentials(){
+	
+	};
+	public static void Validate(){
+	
+	
+	};
+
 }

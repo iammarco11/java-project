f41e65432db2eaf024ef1421afcba9e03138b409
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2020-12-01 19:57:41 +0530

    Income: Initialise all the methods and class variables

--- src/project/Income.java
+++ src/project/Income.java
@@ -1,5 +1,30 @@
 package project;
 
 public class Income {
-
+	String AadhaarNo;
+	double Job;
+	double Business;
+	double Property;
+	double TotalIncome;
+	
+	public Income(String AadhaarNo) {
+		this.AadhaarNo = AadhaarNo;
+	}
+	
+	public double ReturnIncome() {
+		return TotalIncome;
+	}
+	
+	public double Calculation() {
+		return TotalIncome;
+	}
+	
+	public void EditCredentials() {
+		
+	}
+	
+	public void Validate() {
+		
+	}
 }
+

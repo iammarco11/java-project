68e64048bbd3de32245b6a5faaca852fb1eef354
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2020-12-11 02:59:38 +0530

    Income: Fill the necessary functions

--- src/project/Income.java
+++ src/project/Income.java
@@ -1,22 +1,79 @@
 package project;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+
 public class Income {
-	String AadhaarNo;
+	int AadhaarNo;
 	double Job;
 	double Business;
 	double Property;
 	double TotalIncome;
+	Connection connect = null;
 	
-	public Income(String AadhaarNo) {
+	
+	
+	public Income(int AadhaarNo) {
 		this.AadhaarNo = AadhaarNo;
 	}
 	
-	public double ReturnIncome() {
-		return TotalIncome;
+	public int  ReturnIncome(int Username)  {
+		int val = 0;
+		
+		try {
+			Class.forName("org.postgresql.Driver");
+			System.out.println("class done");
+			connect = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tax", "postgres", "root");
+			System.out.println("connect done");
+			PreparedStatement st = (PreparedStatement) connect
+	                .prepareStatement("select total_income from Income where aadhaar_no = ?;");
+			st.setInt(1, Username);
+			ResultSet rs=st.executeQuery();
+			rs.next();
+			val = rs.getInt(1);
+		}
+	catch(Exception e1){
+		e1.printStackTrace();
+        System.exit(0);
+		
 	}
-	
-	public double Calculation() {
-		return TotalIncome;
+		System.out.print(val);
+		System.out.print("Returning val");
+		return val;
+		
+
+
+	}
+	public String Calculation(int Username) {
+		String val = "";
+		
+		try {
+			Class.forName("org.postgresql.Driver");
+			System.out.println("class done");
+			connect = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tax", "postgres", "root");
+			System.out.println("connect done");
+			PreparedStatement st = (PreparedStatement) connect
+	                .prepareStatement("select * from Income where aadhaar_no = ?;");
+			st.setInt(1, Username);
+			ResultSet rs=st.executeQuery();
+			rs.next();
+			val = "" + "Job" + "("+rs.getString(1) + ")"+ "  " + "+" + "  " + "Buissness" + "(" + rs.getString(2) + ")" + "  " + "+" + "  "+"Property" + "(" +
+			
+			rs.getString(3) + ")" + "=" + "  " +rs.getString(5);
+		}
+	catch(Exception e1){
+		e1.printStackTrace();
+        System.exit(0);
+		
+	}
+		System.out.print(val);
+		System.out.print("Returning val");
+		return val;
 	}
 	
 	public void EditCredentials() {
@@ -27,4 +84,5 @@ public class Income {
 		
 	}
 }
+
 

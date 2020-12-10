91cd9a4e18758a5fd03ad08f2aa25228fa4a1d46
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2020-12-11 03:00:05 +0530

    Investment: Fill the necessary functions

--- src/project/Investment.java
+++ src/project/Investment.java
@@ -1,22 +1,51 @@
 package project;
 
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import java.sql.ResultSet;
+
 public class Investment {
 	
-	String AadhaarNo;
+	int AadhaarNo;
 	double HousingLoan;
 	double OtherLoan;
 	double Rent;
 	double ProvidedFund;
 	double Policies;
 	double TotalInvestment;
+	Connection connect = null;
 	
-	
-	public Investment(String AadhaarNo) {
+	public Investment(int AadhaarNo) {
 		this.AadhaarNo = AadhaarNo;
 	}
 	
-	public double ReturnInvestment() {
-		return TotalInvestment;
+	public int ReturnInvestment(int Username) {
+		int val = 0;
+		
+		try {
+			Class.forName("org.postgresql.Driver");
+			System.out.println("class done");
+			connect = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tax", "postgres", "root");
+			System.out.println("connect done");
+			PreparedStatement st = (PreparedStatement) connect
+	                .prepareStatement("select total_investment from investment where aadhaar_no = ?;");
+			st.setInt(1, Username);
+			ResultSet rs=st.executeQuery();
+			rs.next();
+			val = rs.getInt(1);
+		}
+	catch(Exception e1){
+		e1.printStackTrace();
+        System.exit(0);
+		
+	}
+		System.out.print(val);
+		System.out.print("Returning val");
+		return val;
+		
+		
 	}
 	
 	public void EditCredentials() {
@@ -25,9 +54,39 @@ public class Investment {
 	
 	public void Validate() {
 		
+		
 	}
 	
-	public double Calculation() {
-		return TotalInvestment;
+	public String Calculation(int Username) {
+		
+		String val = "";
+		
+		try {
+			Class.forName("org.postgresql.Driver");
+			System.out.println("class done");
+			connect = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tax", "postgres", "root");
+			System.out.println("connect done");
+			PreparedStatement st = (PreparedStatement) connect
+	                .prepareStatement("select * from Investment where aadhaar_no = ?;");
+			st.setInt(1, Username);
+			ResultSet rs=st.executeQuery();
+			rs.next();
+			val = "" + "Provident_fund" + "("+rs.getString(1) + ")"+ "  " + "+" + "  " + "Housing_Loan" + "(" + rs.getString(2) + ")" + "  " + "+" + "  "+"Policies" + "(" +
+			
+			rs.getString(3) + ")" + "  "+ "+" + "  "+ "Other_Loans" + "(" +rs.getString(4) + ")" + "  " +"+" +"  " + "Rent" + "(" +rs.getString(5) + ")" + "  " + "=" + "  " +rs.getString(7);
+		}
+	catch(Exception e1){
+		e1.printStackTrace();
+        System.exit(0);
+		
+	}
+		System.out.print(val);
+		System.out.print("Returning val");
+		return val;
+		
+		
+		
+		
 	}
 }
+
